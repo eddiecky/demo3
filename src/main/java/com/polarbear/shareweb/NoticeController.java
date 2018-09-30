8ded91581c310994bd313e1bec33287ab731af70
Eddie Cheung <eddiecky@gmail.com>
2018-09-30 09:18:25 +0800

    1.0.1

--- src/main/java/com/polarbear/shareweb/NoticeController.java
+++ src/main/java/com/polarbear/shareweb/NoticeController.java
@@ -2,7 +2,6 @@ package com.polarbear.shareweb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
