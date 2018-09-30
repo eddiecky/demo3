fbf68417e6749fe0cf946957f9cdd73e612a4b85
Eddie Cheung <eddiecky@gmail.com>
2018-09-30 10:04:54 +0800

    1.0.3

--- src/main/java/com/polarbear/shareweb/NoticeController.java
+++ src/main/java/com/polarbear/shareweb/NoticeController.java
@@ -1,5 +1,9 @@
 package com.polarbear.shareweb;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.tomcat.jni.File;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class NoticeController {
 
-    @RequestMapping(value={"/updateMsg"}, method = RequestMethod.POST)
+    @RequestMapping(value = { "/updateMsg" }, method = RequestMethod.POST)
     public String updateMsg(Model model, @RequestParam("systemMessage") String message) {
+
         model.addAttribute("message", message);
+        FileWriter file = null;
+
+        try {
+            file = new FileWriter("/data/message.json");
+            file.write(message);
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         return "notice";
     }
 
